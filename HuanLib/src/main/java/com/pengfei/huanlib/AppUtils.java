6dc6bcb7e05f6c58434e4eb8799d7fdea0076cda
wanghuanlong <wanghuanlong1995@qq.com>
2021-02-15 17:05:32 +0800

    方法整理，删除错误、移动分类
    (cherry picked from commit 995df494e4e91a2751d7dd7f23473d53588a9a5b)

--- HuanLib/src/main/java/com/pengfei/huanlib/AppUtils.java
+++ HuanLib/src/main/java/com/pengfei/huanlib/AppUtils.java
@@ -1,14 +1,11 @@
 package com.pengfei.huanlib;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
@@ -91,47 +88,25 @@ public class AppUtils {
         return versioncode;
     }
 
-    public static void jumpQQ(Activity mActivity, String qqNum) {
-        if (mActivity == null)
-            return;
-        try {
-            mActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("mqqwpa://im/chat?chat_type=wpa&uin=" + qqNum + "&version=1")));
-        } catch (Exception e) {
-
-        }
-    }
-
-
-    /**
-     * 拨打电话（跳转到拨号界面，用户手动点击拨打）
-     *
-     * @param phoneNum 电话号码
-     */
-    public static void callPhone(Context mActivity, String phoneNum) {
-        if (mActivity == null)
-            return;
-        try {
-            Intent intent = new Intent(Intent.ACTION_DIAL);
-            Uri data = Uri.parse("tel:" + phoneNum);
-            intent.setData(data);
-            mActivity.startActivity(intent);
-        } catch (Exception e) {
-        }
-    }
-
-
-    public static void sendMail(Context context, String email) {
-        Uri uri = Uri.parse("mailto:" + email);
-        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
-        context.startActivity(Intent.createChooser(intent, "请选择发送邮件的应用"));
-    }
-
     public static String getPackageName(Context context) {
         try {
             return context.getPackageName();
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public static String getProcessName(Context cxt, int pid) {
+        ActivityManager am = (ActivityManager) cxt.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> runningApps = am.getRunningAppProcesses();
+        if (runningApps != null && !runningApps.isEmpty()) {
+            for (ActivityManager.RunningAppProcessInfo procInfo : runningApps) {
+                if (procInfo.pid == pid) {
+                    return procInfo.processName;
+                }
+            }
+        }
+        return null;
     }
 
     /**
@@ -161,15 +136,7 @@ public class AppUtils {
         listView.requestLayout();
     }
 
-    @SuppressLint("NewAPi")
-    public static boolean CopyLink(Context mActivity, String text) {
-        ClipboardManager cm = (ClipboardManager) mActivity.getSystemService(Context.CLIPBOARD_SERVICE);
-        if (!text.isEmpty()) {
-            cm.setText(text);
-            return true;
-        }
-        return false;
-    }
+
 
     public static String getChannel(Context context) {
         String channel = "test";
@@ -183,18 +150,5 @@ public class AppUtils {
             e.printStackTrace();
         }
         return "test";
-    }
-
-    public static String getProcessName(Context cxt, int pid) {
-        ActivityManager am = (ActivityManager) cxt.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> runningApps = am.getRunningAppProcesses();
-        if (runningApps != null && !runningApps.isEmpty()) {
-            for (ActivityManager.RunningAppProcessInfo procInfo : runningApps) {
-                if (procInfo.pid == pid) {
-                    return procInfo.processName;
-                }
-            }
-        }
-        return null;
     }
 }
--- HuanLib/src/main/java/com/pengfei/huanlib/system/SystemUtil.java
+++ HuanLib/src/main/java/com/pengfei/huanlib/system/SystemUtil.java
@@ -1,7 +1,11 @@
 package com.pengfei.huanlib.system;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -25,5 +29,48 @@ public class SystemUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 拨打电话（跳转到拨号界面，用户手动点击拨打）
+     *
+     * @param phoneNum 电话号码
+     */
+    public static void callPhone(Context mActivity, String phoneNum) {
+        if (mActivity == null)
+            return;
+        try {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            Uri data = Uri.parse("tel:" + phoneNum);
+            intent.setData(data);
+            mActivity.startActivity(intent);
+        } catch (Exception e) {
+        }
+    }
+
+    public static void sendMail(Context context, String email) {
+        Uri uri = Uri.parse("mailto:" + email);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        context.startActivity(Intent.createChooser(intent, "请选择发送邮件的应用"));
+    }
+
+    public static void jumpQQ(Activity mActivity, String qqNum) {
+        if (mActivity == null)
+            return;
+        try {
+            mActivity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("mqqwpa://im/chat?chat_type=wpa&uin=" + qqNum + "&version=1")));
+        } catch (Exception e) {
+
+        }
+    }
+
+    @SuppressLint("NewAPi")
+    public static boolean CopyLink(Context mActivity, String text) {
+        ClipboardManager cm = (ClipboardManager) mActivity.getSystemService(Context.CLIPBOARD_SERVICE);
+        if (!text.isEmpty() && cm != null) {
+            cm.setText(text);
+            return true;
+        }
+        return false;
     }
 }
